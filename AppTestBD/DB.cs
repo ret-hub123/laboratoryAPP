c68dd22c14781568d1d936e8142ce23cba3d1597
CrivayCapcha <tedcruz12345q@gmail.com>
2025-03-22 22:24:52 +0300

    Добавьте файлы проекта.

---
+++ AppTestBD/DB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace AppTestBD
+{
+    class DB
+    {
+        
+        static void Connect()
+        {
+
+            string connectionString = @"Source = DESKTOP - KSH314B\MSSQLSERVER1; Initial Catalog = laboratory data; Integrated Security = True; Encrypt = False";
+       
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+             
+                MessageBox.Show("SELECT * FROM Patients");
+            }
+            Console.WriteLine("Подключение закрыто...");
+
+            Console.Read();
+        }
+    }
+}
